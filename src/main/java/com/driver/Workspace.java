7875f9c979864330af0cfec744a2ae23b423acf0
akankshamakkar <akimakkar1282@gmai.com>
2023-05-14 16:02:05 +0530

    second

--- src/main/java/com/driver/Workspace.java
+++ src/main/java/com/driver/Workspace.java
@@ -1,9 +1,12 @@
 package com.driver;
 
 import org.apache.commons.lang3.tuple.Pair;
+
+import javax.lang.model.type.ArrayType;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 public class Workspace extends Gmail{
 
@@ -25,29 +28,33 @@ public class Workspace extends Gmail{
         // at a particular time, you can only be present in at most one meeting
         // if you want to attend a meeting, you must join it at its start time and leave at end time.
 
-        ArrayList<Pair<LocalTime, Integer>> endTimes = new ArrayList<>();
+        List<Meeting> sortedCalender = List.copyOf(calendar);
+        Collections.sort(sortedCalender, (x,y) -> x.getEndTime().compareTo(y.getEndTime()) );
 
-        for (int i = 0; i < calendar.size(); i++) {
-            endTimes.add(Pair.of(calendar.get(i).getEndTime(), i));
-        }
+//        ArrayList<Pair<LocalTime, Integer>> endTimes = new ArrayList<>();
 
-        Collections.sort(endTimes);
+//        for (int i = 0; i < calendar.size(); i++) {
+//            endTimes.add(Pair.of(calendar.get(i).getEndTime(), i));
+//        }
+//
+//        Collections.sort(endTimes);
+//
+//        LocalTime time_limit = endTimes.get(0).getLeft();
+        List<Meeting> maxMeetings = new ArrayList<>();
+        LocalTime time_limit = sortedCalender.get(0).getStartTime();
+//        int cnt = 0;
+//        if(!sortedCalender.isEmpty()) {
+//            cnt += 1;
+//        }
 
-        LocalTime time_limit = endTimes.get(0).getLeft();
 
-        int cnt = 0;
-        if(!endTimes.isEmpty()) {
-            cnt += 1;
-        }
-
-        for (int i = 1; i < endTimes.size(); i++) {
-            if (calendar.get(endTimes.get(i).getRight()).getStartTime().compareTo(time_limit) > 0) {
-
-                cnt += 1;
-                time_limit = endTimes.get(i).getLeft();
+        for (Meeting meeting: sortedCalender) {
+            if (meeting.getStartTime().compareTo(time_limit) >= 0) {
+                maxMeetings.add(meeting);
+                time_limit = meeting.getStartTime();
             }
         }
 
-        return cnt;
+        return maxMeetings.size();
     }
 }
